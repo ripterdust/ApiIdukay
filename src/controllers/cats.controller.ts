fa30a9450a62213f5b205f340754750b66ee243a
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 10:55:21 -0600

    :rocket: Getting the Cats data

--- src/controllers/cats.controller.ts
+++ src/controllers/cats.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express'
 import { RouteResponse } from '../common/interfaces/response.interface'
+import { Cats } from '../models/cats.model'
 
 export const indexCats = async (req: Request, res: Response) => {
+    const data = await Cats.get()
+
     const response: RouteResponse = {
         statuscode: 200,
         message: 'Cats collection',
+        data,
     }
 
     res.status(response.statuscode).json(response)
