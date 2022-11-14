d26c9273b0c03572f76803fd203d49a8c8627058
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 10:49:09 -0600

    :rocket: Agregada la estructura de los gatos

--- src/routers/cats.routes.ts
+++ src/routers/cats.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { Route } from '../common/interfaces/route.interface'
+import { indexCats } from '../controllers/cats.controller'
 
 const router = Router()
+
+router.get('/', indexCats)
 
 export const catsRoutes: Route = {
     endpoint: '/cats',
