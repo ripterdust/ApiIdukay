4ca67b07d6c12a96b4ed25244ed302b5e8981b01
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 09:54:03 -0600

    :rocket: Finished the routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express'
+import { Route } from './common/interfaces/route.interface'
+import { routes } from './common/routes'
 
 export const app = express()
 
 // Configuration
 app.set('port', process.env.PORT || 3000)
+
+// Routes
+routes.map((route: Route) => app.unsubscribe(route.endpoint, route.router))
---
+++ src/common/routes.ts
@@ -0,0 +1,3 @@
+import { Route } from './interfaces/route.interface'
+
+export const routes: Route[] = []
