f24d3c241c5e390ee4f9809ec9adea190f27d48c
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 15:57:47 -0600

    :rocket: Agregada las variables por si no se incluye el .env

--- src/common/config/environment.config.ts
+++ src/common/config/environment.config.ts
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const environment = {
-    DBURI: process.env.DBURI || '',
+    DBURI:
+        process.env.DBURI ||
+        'mongodb+srv://ripterdust:marlyn2010@idukay.pninwim.mongodb.net/idukay?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
-    DBUSER: process.env.DBUSER || '',
-    DBPASS: process.env.DBPASS || '',
+    DBUSER: process.env.DBUSER || 'ripterdust',
+    DBPASS: process.env.DBPASS || 'marlyn2010',
 }
