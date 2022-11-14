b380583e2f69e318087a19b8f77f7c70192a8026
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 10:05:19 -0600

    :rocket: Added the main router logic

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -1,4 +1,6 @@
+import { catsRoutes } from '../routers/cats.routes'
+import { dogsRoutes } from '../routers/dogs.routes'
 import { indexRoutes } from '../routers/index.routes'
 import { Route } from './interfaces/route.interface'
 
-export const routes: Route[] = [indexRoutes]
+export const routes: Route[] = [indexRoutes, dogsRoutes, catsRoutes]
--- src/routers/cats.routes.ts
+++ src/routers/cats.routes.ts
@@ -3,7 +3,7 @@ import { Route } from '../common/interfaces/route.interface'
 
 const router = Router()
 
-export const dogsRoutes: Route = {
-    endpoint: '/dogs',
+export const catsRoutes: Route = {
+    endpoint: '/cats',
     router,
 }
